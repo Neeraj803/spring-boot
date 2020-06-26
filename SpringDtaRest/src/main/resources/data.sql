692222964acd327aaa592b91a62fe30504f8ef6c
unknown <Neeraj Lodhi>
2020-06-26 18:36:41 +0530

    Rest Project

---
+++ SpringDtaRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(1,'Neeraj','java');
+insert into alien values(2,'Shikha','java');
+insert into alien values(3,'Amit','java');
+insert into alien values(4,'Chanchal','java');
+insert into alien values(5,'Ram','java');
